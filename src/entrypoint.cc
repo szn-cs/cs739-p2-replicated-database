ae1c15f4bed5ce9ca49120eaa70099cda8c067d0
Safi <nassarsafi@gmail.com>
2023-04-09 19:27:12 -0500

    Merge branch 'main' of https://github.com/szn-cs/cs739-p2-replicated-database

--- src/entrypoint.cc
+++ src/entrypoint.cc
@@ -213,8 +213,8 @@ int user_entrypoint(std::shared_ptr<utility::parse::Config> config, boost::progr
 
       std::map<std::string, std::vector<std::string>> results;
       for (unsigned long i = 0; i < db_addrs.size(); i++) {
-        google::protobuf::Map<string,string> res = db_addrs[i]->get_db();
-        for (auto & kb : res) {
+        google::protobuf::Map<string, string> res = db_addrs[i]->get_db();
+        for (auto& kb : res) {
           results[str_addrs[i]].push_back(kb.second);
         }
       }
@@ -265,14 +265,14 @@ int user_entrypoint(std::shared_ptr<utility::parse::Config> config, boost::progr
         int replica_idx = rand() % db_addrs.size();
         int key_idx = rand() % keys.size();
         Status s = db_addrs[replica_idx]->set(keys[key_idx], result);
-        if(!s.ok()){
-          std::cout << reset << red << "Set(" << keys[key_idx] << ", " << result << ") failed" << reset << endl; 
+        if (!s.ok()) {
+          std::cout << reset << red << "Set(" << keys[key_idx] << ", " << result << ") failed" << reset << endl;
         }
       }
 
       std::map<std::string, std::vector<std::string>> results;
       for (int i = 0; i < 5; i++) {
-        google::protobuf::Map<string,string> r = db_addrs[i]->get_db();
+        google::protobuf::Map<string, string> r = db_addrs[i]->get_db();
         results[str_addrs[i]];
         for (std::string key : keys) {
           results[str_addrs[i]].push_back(r[key]);
@@ -284,7 +284,6 @@ int user_entrypoint(std::shared_ptr<utility::parse::Config> config, boost::progr
         copy(value.begin(), value.end(), ostream_iterator<std::string>(std::cout, " "));
         std::cout << reset << endl;
       }
-<<<<<<< HEAD
     } else if (command == "test_count") {
       cout << "inCount: " << endl;
 
@@ -302,9 +301,6 @@ int user_entrypoint(std::shared_ptr<utility::parse::Config> config, boost::progr
                   << x.second  // string's value
                   << std::endl;
       }
-=======
-
->>>>>>> e3a41b1068a3f4c10c93ac4a77ecd68b8f988ff7
     }
   }
 
